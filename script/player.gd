223d4ad2ff9f323e8cf8df5f7a4816b02ac40c49
Jacob Adams <tookmund@gmail.com>
2017-07-06 23:18:50 -0400

    player.gd: Calc contact with ground via oldstate
    Still laggy but better than before

--- script/player.gd
+++ script/player.gd
@@ -4,6 +4,7 @@ var dx = 0
 var dy = 0
 signal released
 signal pressed
+var contact
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -14,6 +15,10 @@ func _ready():
 
 var move = 5
 func _input(event):
+	if (!contact):
+		dx = 0
+		dy = 0
+		return
 	if (event.is_action_pressed("move_left")):
 		dx = -move
 		emit_signal("pressed")
@@ -30,9 +35,20 @@ func _input(event):
 		dy = 0
 		emit_signal("released")
 
+var maxvel = 300
 func _integrate_forces(state):
+		contact = state.get_contact_count() > 0
 		if (dx != 0 or dy != 0):
 			var vel = state.get_linear_velocity()
 			vel.x += dx
+			if (vel.x > maxvel):
+				vel.x = maxvel
+			elif (vel.x < -maxvel):
+				vel.x = -maxvel
 			vel.y += dy
+			if (vel.y > maxvel):
+				vel.y = maxvel
+			elif (vel.y < -maxvel):
+				vel.y = -maxvel
+			print(vel.x," ",vel.y)
 			state.set_linear_velocity(vel)
